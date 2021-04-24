11b26017931e8a0201504bdfda96423b957def4d
EslamKhder <eslamkhder81@gmail.com>
2021-04-24 02:59:13 +0200

    create PlayerController

---
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -0,0 +1,13 @@
+package com.spring.playerapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// http://localhost:8080
+@RestController
+@RequestMapping("/api")
+// http://localhost:8080/api
+public class PlayerController {
+
+}
