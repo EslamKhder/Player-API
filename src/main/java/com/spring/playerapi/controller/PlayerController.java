f91e2a4e17203df3a0e32d6131f53c646a5f4c6e
EslamKhder <eslamkhder81@gmail.com>
2021-04-24 23:44:23 +0200

    Update Project

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -1,5 +1,6 @@
 package com.spring.playerapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 // http://localhost:8080/api
 public class PlayerController {
-
+	
+	@GetMapping("/d")
+	public String getting() {
+		return "d";
+	}	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+
+<h3>Welcome !</h3>
+
 </body>
 </html>
