e3c7ec90423d3806e0c2a7a09ebd4db1d85c8773
EslamKhder <eslamkhder81@gmail.com>
2021-04-25 02:16:26 +0200

    get player by id from DB

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.spring.playerapi.model.Player;
 import com.spring.playerapi.service.PlayerService;
@@ -34,4 +36,16 @@ public class PlayerController {
 		return playerService.allPlayers();
 	}
 	
+	
+	// http://localhost:8080/spring-crm-rest/api/players/id
+	@GetMapping("/players/{id}")
+	public Player getPlayerById(@PathVariable("id")int id) {
+		return playerService.showPlayer(id);
+	}
+	
+	// http://localhost:8080/spring-crm-rest/api/players
+	/*@GetMapping("/players")
+	public Player getPlayerById(@RequestParam int id) {
+		return playerService.showPlayer(id);
+	}*/
 }
