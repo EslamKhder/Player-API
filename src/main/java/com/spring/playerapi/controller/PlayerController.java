5fc5e0f5c75531e12778254566da949c17e1d291
EslamKhder <eslamkhder81@gmail.com>
2021-04-25 03:14:54 +0200

    Edit Player

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,4 +65,10 @@ public class PlayerController {
 		return "Success Added";
 	}
 	
+	// http://localhost:8080/spring-crm-rest/api/players
+	@PutMapping("/players")
+	public String editPlayer(@RequestBody Player player) {
+		playerService.savePlayer(player);
+		return "Success editing";
+	}
 }
